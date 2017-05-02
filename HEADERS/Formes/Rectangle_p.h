c664710d1fe81cc1a653d6c5f3ed545efb171fc7
Cahmcam <Cml.magnan@gmail.com>
2017-05-02 18:39:17 +0200

    Rectangle_p

--- HEADERS/Formes/Rectangle_p.h
+++ HEADERS/Formes/Rectangle_p.h
@@ -1,4 +1,4 @@
-// Clovis Durand & Camille Magnan
+/*// Clovis Durand & Camille Magnan
 // ENSEIRB-Matmeca, E2
 // 2017
 
@@ -59,3 +59,45 @@ private :
 
 
 #endif 
+*/
+
+	// Clovis Durand & Camille Magnan
+// ENSEIRB-Matmeca, E2
+// 2017
+
+// PG208 - Projet Dessin Vectoriel
+
+// Rectangle_p.h
+// Rectangle_p class header file. 
+
+#include "Rectangle_p.h"
+
+
+class Rectangle_p : public Rectangle
+{
+public:
+	// Con/Destructeurs
+
+	Rectangle_p(Coord const c1, 
+	unsigned int const longueur, 
+	unsigned int const hauteur,  
+	std::string const couleur, 
+	unsigned int const transparence);
+
+
+	// Afficheurs
+
+	// Accesseurs
+
+	// Mutateurs
+
+	// Draw
+	draw(CImage* Image);
+
+	// Méthodes publiques Diverses
+
+//private:
+	// Attributs/Variables membres
+
+	// Méthodes privées diverses 
+}
--- SOURCES/Formes/Rectangle_p.cpp
+++ SOURCES/Formes/Rectangle_p.cpp
@@ -1,4 +1,4 @@
-// Clovis Durand & Camille Magnan
+/*// Clovis Durand & Camille Magnan
 // ENSEIRB-Matmeca, E2
 // 2017
 
@@ -49,4 +49,47 @@ for(i = Coord1.getAbscisse(); i >= longueur; i++)
 		p -> RGB(p->Red(),p->Green(),p->Blue());
 	}
 }
+}*/
+
+// Clovis Durand & Camille Magnan
+// ENSEIRB-Matmeca, E2
+// 2017
+
+// PG208 - Projet Dessin Vectoriel
+
+// Rectangle_p.cpp
+
+#include "../../HEADERS/Formes/Rectangle_p.h"
+
+// Con/Destructeurs
+
+// Afficheurs
+
+// Accesseurs
+
+// Mutateurs
+
+// Draw
+Rectangle_p::draw(CImage *img)
+{
+	setRGBcolor()
+int i, j;
+int origine_y = getCoord1().getOrdonee();
+int origine_x = getCoord1().getAbscisse();
+int longueur = getLigne2().getCoord1().getOrdonee() - origine_y ;
+int hauteur = getLigne3().getCoord1().getAbscisse() - origine_x ;
+
+for(i = origine_x; i >= longueur; i++)
+{
+	for(j = origine_y; j>= hauteur; j++)
+	{
+		CPixel *p = Image->getPixel(i, j);
+		p -> RGB(p->Red(),p->Green(),p->Blue());
+	}
 }
+
+// Méthodes publiques Diverses
+
+// Attributs/Variables membres
+
+// Méthodes privées diverses 
